c9d08ae087777e511a9bdb752d25253b900d94ab
John Saputo <jsaputo1@gmail.com>
2020-06-04 19:11:09 +0000

    exported feedBreedDescription function and moved console log messages to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,15 @@
-const args = process.argv.slice(2);
-const searchParameter = args[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${searchParameter}`;
-
 const request = require("request");
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(data[0].description);
-});
+
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length < 1) {
+      callback("No data available", null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
